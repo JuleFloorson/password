39bf5b0f3243e145a3fefeb0a4bb2a317ce9ef60
JuleFloorson <jubo93@web.de>
2020-03-23 17:37:58 +0100

    Export get set unset

--- app.js
+++ app.js
@@ -1,50 +1,12 @@
 const [command, key, value] = process.argv.slice(2);
-const { read, write } = require("./lib/passwords");
-
-function get() {
-  console.log("Called GET", key);
-  // Read and log db.json
-  try {
-    const passwords = read();
-    console.log(key, passwords[key]);
-  } catch (error) {
-    console.error(error);
-    console.log("db");
-  }
-}
-
-function set() {
-  console.log("Called SET", key, value);
-
-  try {
-    // Read db.json
-    const passwords = read();
-    // Update value by key
-    passwords[key] = value;
-    // Write db.json
-    write(passwords);
-    console.log(key, passwords[key]);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function unset() {
-  // Read db.json
-  const passwords = read();
-  //Remove value by key
-  delete passwords[key];
-  // Write db.json
-  write(passwords);
-  console.log(passwords);
-}
+const { get, set, unset } = require("./lib/commands");
 
 if (command === "get") {
-  get();
+  get(key);
 } else if (command === "set") {
-  set();
+  set(key, value);
 } else if (command === "unset") {
-  unset();
+  unset(key);
 } else {
   console.error("Unkown command");
 }
