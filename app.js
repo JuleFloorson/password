0867bd84136b033983facdc90f6d91a095697135
JuleFloorson <jubo93@web.de>
2020-03-23 16:32:09 +0100

    Add unset passwords

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const fs = require("fs");
 const [command, key, value] = process.argv.slice(2);
 
 function get() {
   console.log("Called GET", key);
   // Read and log db.json
   try {
-    const passwordsJSON = fs.readFileSync("./db.json", "utf8");
-    const passwords = JSON.parse(passwordsJSON);
+    const passwords = readPasswords();
     console.log(key, passwords[key]);
   } catch (error) {
     console.error(error);
@@ -19,24 +17,33 @@ function set() {
 
   try {
     // Read db.json
-    const passwordsJSON = fs.readFileSync("./db.json", "utf8");
-    const passwords = JSON.parse(passwordsJSON);
-
+    const passwords = readPasswords();
     // Update value by key
     passwords[key] = value;
-    console.log(key, passwords[key]);
-
     // Write db.json
-    fs.writeFileSync("./db.json", JSON.stringify(passwords, null, 10));
+    writePasswords();
+    console.log(key, passwords[key]);
   } catch (error) {
     console.error(error);
   }
+}
+
+function unset() {
+  // Read db.json
+  const passwords = readPasswords();
+  //Remove value by key
+  delete passwords[key];
+  // Write db.json
+  writePasswords();
+  console.log(passwords);
 }
 
 if (command === "get") {
   get();
 } else if (command === "set") {
   set();
+} else if (command === "unset") {
+  unset();
 } else {
   console.error("Unkown command");
 }
---
+++ passwords.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+function readPasswords() {
+  const passwordsJSON = fs.readFileSync("./db.json", "utf8");
+  const passwords = JSON.parse(passwordsJSON);
+  return passwords;
+}
+
+function writePasswords(passwords) {
+  fs.writeFileSync("./db.json", JSON.stringify(passwords, null, 4));
+}
