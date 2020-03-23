e2388e501a451a9067f99484ee4e89622265a2e2
JuleFloorson <jubo93@web.de>
2020-03-23 14:40:44 +0100

    Add get filesystem

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Irgendwas lustiges!");
+const fs = require("fs");
+const [command, key, value] = process.argv.slice(2);
+
+function get() {
+  console.log("Called GET", key);
+  // Read and log db.json
+  try {
+    const passwordsJSON = fs.readFileSync("db.json", "utf8");
+    const passwords = JSON.parse(passwordsJSON);
+    console.log(key, passwords[key]);
+  } catch (error) {
+    console.error(error);
+    console.log("db");
+  }
+}
+
+function set() {
+  console.log("Called SET", key, value);
+  // Read db.json
+  // Update value by key
+  // Write db.json
+}
+
+if (command === "get") {
+  get();
+} else if (command === "set") {
+  set();
+} else {
+  console.error("Unkown command");
+}
