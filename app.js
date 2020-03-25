a283843fee7423056e08d0cf58bb53fd04a0dd67
JuleFloorson <jubo93@web.de>
2020-03-25 09:55:38 +0100

    Add master password reset

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const { get, set, unset } = require('./lib/commands');
+const { get, set, unset, reset } = require('./lib/commands');
 const { askForPassword, askForMasterPassword } = require('./lib/questions');
 const { readMasterPassword } = require('.lib/passwords');
-
+const { verifyHash } = require('./lib/crypto');
 const [command, key] = process.argv.slice(2);
 
 async function run() {
+  // we need a new command to set a new master password
+  //this command will read the password and save the hash in db.json
+
   const answeredMasterPassword = await askForMasterPassword();
+
+  if (command === 'reset') {
+    return reset(answeredMasterPassword);
+  }
+
   const masterPassword = readMasterPassword();
 
-  if (answeredMasterPassword !== masterPassword) {
+  //masterpassword should be the hash of the original password
+  //we need to hash the answered master password too
+  // and then we can compare both hashes
+
+  if (!verifyHash(answeredMasterPassword !== masterPassword)) {
     console.error('Fuck off');
     return;
   }
